3da0ef79d58f8597ed310dfd1567910d29a8d338
CindyMisoi <crmisoi2k@gmail.com>
2022-06-30 03:18:34 -0700

    print statement with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * print statement using printf
+ *
+ * Return - always (0)
+ */
+
+int void  (main)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	           return (0);
+}
