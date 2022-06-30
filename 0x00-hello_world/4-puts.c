e55ad3c68db9ea47e2fb3745778890aee72b203d
CindyMisoi <crmisoi2k@gmail.com>
2022-06-30 03:05:08 -0700

    print code using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * print /puts output 
+ *
+ * return the value 0
+ * */
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
