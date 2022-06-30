a0e6922485c00290b4906697d222b381e705251e
CindyMisoi <crmisoi2k@gmail.com>
2022-06-30 03:22:36 -0700

    print statement with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry
- *
- * print statement using printf
- *
- * Return - always (0)
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
  */
-
-int void  (main)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	           return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
