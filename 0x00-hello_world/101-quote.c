3d948a4a010148f332a8b36f4c4589680f8fc369
CindyMisoi <crmisoi2k@gmail.com>
2022-06-30 23:42:46 -0700

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
+ *
  * Return: 1 if success.
+ *
  */
 int main(void)
 {
 		write(2, "and that piece of art is useful - Dora Korpar, 2015-1
-				0-19\n", 58);
+0-19\n", 58);
 			return (1);
 }
