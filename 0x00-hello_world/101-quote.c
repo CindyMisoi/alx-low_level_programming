5abf8fdce0605126b4e2f248b2196f093bb6124e
CindyMisoi <crmisoi2k@gmail.com>
2022-06-30 23:25:19 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,12 @@
 /**
  * main - entry point
  *
- * Return Always 1 (success)
+ * Return 1 if success
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-		       	58);
-	return (1);
-       
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-15\n", 
+        58);
+           	return (1);
 }       
