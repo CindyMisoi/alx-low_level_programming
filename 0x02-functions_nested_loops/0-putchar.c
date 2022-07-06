ec7e3aeedc62580212c1f629d4167b993752362b
CindyMisoi <crmisoi2k@gmail.com>
2022-07-05 21:48:10 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	char *sh = "main";
+	
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++:
+	}
+	
+	_putchar('\n');
+
+	return (0);
+}
