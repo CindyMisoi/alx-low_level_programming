4e52c18744aee4b8c0280b362237e564ca41299f
CindyMisoi <crmisoi2k@gmail.com>
2022-07-07 21:23:22 -0700

    print alphabet letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
