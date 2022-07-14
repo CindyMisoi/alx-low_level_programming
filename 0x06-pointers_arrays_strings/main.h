dfd720dc71baf57c3f10ad569782bd5114327f6c
CindyMisoi <crmisoi2k@gmail.com>
2022-07-14 10:44:41 -0700

    changes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@
  */
 
 int _putchar(char c);
+char *strcat(char *dest, const char *src)
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
