eee6b5dcf14acea51f1cc45823b16fe012b8d341
CindyMisoi <crmisoi2k@gmail.com>
2022-07-14 11:07:12 -0700

    changes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,9 +7,7 @@
  * Desc: Header file containing prototypes for all functions
  * used in the 0x05-pointers_arrays_strings directory.
  */
-
 int _putchar(char c);
-char *strcat(char *dest, const char *src)
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
