27dc6693b67e0ddf1f993e8a0ff6bc535c31bea0
CindyMisoi <crmisoi2k@gmail.com>
2022-07-14 21:22:29 -0700

    changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcat(char *dest, const char *src)
+char *strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
