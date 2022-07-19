a207fd85e46f4a2aa30a2d927e789692e4ac7818
CindyMisoi <crmisoi2k@gmail.com>
2022-07-18 22:03:30 -0700

    copy in memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,4 +9,14 @@
  *
  * Return: A pointer to the destination buffer @dest.
  */
-void *_memcpy(void *dest, cost void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+	char *destination = dest;
+	char *source  = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
+
+	return (dest);
+}
