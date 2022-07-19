4ce83f31ecd52385c59b429b633a68cf2fdc040d
CindyMisoi <crmisoi2k@gmail.com>
2022-07-18 21:57:10 -0700

    main.h file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies @n bytes from the memory area pointed
+ * to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src.
+ *
+ * Return: A pointer to the destination buffer @dest.
+ */
+void *_memcpy(void *dest, cost void *src, size_t n)
