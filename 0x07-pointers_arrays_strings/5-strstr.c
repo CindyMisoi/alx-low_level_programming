1552a86cb4c0168c215af4dbaf7d200d2957ac5d
CindyMisoi <crmisoi2k@gmail.com>
2022-07-18 23:34:25 -0700

    changes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,10 +25,10 @@ char *_strstr(char *haystack, char *needle)
 			do {
 				if (needle[index + 1] == '\0')
 					return (haystack);
-
+				
 				index++;
 
-			}while (haystack[index] == needle[index]);
+			} while (haystack[index] == needle[index]);
 		}
 
 		haystack++;
