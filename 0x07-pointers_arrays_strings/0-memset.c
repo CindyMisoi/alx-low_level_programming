14ab394c5a5f4f7e4fa3dcb7eb57e435abdf6aa0
CindyMisoi <crmisoi2k@gmail.com>
2022-07-18 22:10:00 -0700

    changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,18 +2,18 @@
 
 /**
  * _memset - fills the first n byte of the memory area
- * pointed to by @s with constant byte @c
+ * pointed to by @s with constant byte @b
  * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
+ * @b: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
  * Return: A pointer to the filled memory area @s.
  *
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int index;
+	char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
